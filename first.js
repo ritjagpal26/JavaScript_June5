f85e7a12bef6eeb84c0d07a151d935fd8d8d593e
Ritik Jagpal <55303144+ritjagpal26@users.noreply.github.com>
2020-06-05 10:55:06 -0400

    2
    2

--- first.js
+++ first.js
@@ -1,3 +1,6 @@
+// REPORT on the SUM of all numbers from 1000 going down to 17Count down by 11 each time
+// ADD UP all the numbers you step over as you are counting down from 1000,
+//  going down 11 each time, until you get to 17Output the result
 var sum = 0;
 for (var i=1000;i>=17;i-=11) {
     sum += i
