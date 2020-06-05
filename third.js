daa81a22433eb5c92079a0fe523f44ac92a1c0f3
Ritik Jagpal <55303144+ritjagpal26@users.noreply.github.com>
2020-06-05 11:32:34 -0400

    1
    1

---
+++ third.js
@@ -0,0 +1,21 @@
+//Sum of numbers using for and while loop
+sum = 0;
+
+for( i=0;i<10;i++)
+{
+sum+=i;
+
+if(i==5)
+break;
+}
+console.log(" sum is = "+ sum);
+var sum1 = 0;
+j = 0 ;
+while(j<=5)
+{
+    sum1+=j;
+      j++;
+}
+console.log("sum for while loop is = "+ sum1)
+
+
